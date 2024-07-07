c8e1d7504336ea325d4516a8edff58ca61dcd827
junhao8851 <yangjunhao2029@163.com>
2024-07-07 23:14:34 +0800

    Add files via upload
    堆栈，stack.pop(),stack.append()

---
+++ 05_ 有效括号字符串.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        lookup = {"{":"}","(":")","[":"]"}
+        for bracket in s:
+            if bracket in lookup:
+                stack.append(bracket)
+            elif  len(stack) == 0 or lookup[stack.pop()] != bracket:
+                return False      
+        return len(stack) == 0
+    s=Solution()
+    print(s.isValid("{(}[)]"))
