002b680163f10aad2da526113b2d7ea9e21b1f74
junhao8851 <yangjunhao2029@163.com>
2024-08-21 21:13:18 +0800

    Add files via upload

---
+++ 栈、二叉树/有效括号.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for item in s:
+            if item == '(':
+                stack.append(')')
+            elif item == '[':
+                stack.append(']')
+            elif item == '{':
+                stack.append('}')
+            elif not stack or stack[-1] != item:
+                return False
+            else:
+                stack.pop()
+        
+        if not stack:
+            return True
+        else:
+            return False 
