33d352979a2a4788776c23c5189126a9dafddd89
junhao8851 <yangjunhao2029@163.com>
2024-09-13 22:21:51 +0800

    Add files via upload

---
+++ 动态规划/目标和.py
@@ -0,0 +1,21 @@
+#01背包的常见变式：
+#1.最多装capacity，求最大价值/方案数量
+#2.最少装capacity，求最小价值/方案数量
+#3.恰好装capacity，求最大/最小价值/方案数量
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+         s = sum(nums) - abs(target)
+         if s < 0 or s % 2:
+            return 0
+         m = s // 2  # 背包容量
+         
+         @cache 
+
+         def dfs(i: int, c: int) -> int:
+            if i < 0:
+                return 1 if c == 0 else 0
+            if c < nums[i]:
+                return dfs(i - 1, c)  # 只能不选
+            return dfs(i - 1, c) + dfs(i - 1, c - nums[i])  # 不选 + 选
+
+         return dfs(len(nums) - 1, m)
