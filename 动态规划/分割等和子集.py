ba904dbc6d807965b8025b2903cdcee7005a1e89
junhao8851 <yangjunhao2029@163.com>
2024-09-15 20:24:44 +0800

    Add files via upload

---
+++ 动态规划/分割等和子集.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        _sum = 0
+
+        # dp[i]中的i表示背包内总和
+        dp = [0] * 10001
+        for num in nums:
+            _sum += num
+        # 也可以使用内置函数一步求和
+        # _sum = sum(nums)
+        if _sum % 2 == 1:
+            return False
+
+        target = _sum // 2
+
+        # 开始 01背包
+        for num in nums:
+            for j in range(target, num - 1, -1):  # 每一个元素一定是不可重复放入，所以从大到小遍历
+                dp[j] = max(dp[j], dp[j - num] + num)
+
+        # 集合中的元素正好可以凑成总和target
+        if dp[target] == target:
+            return True
+        return False
