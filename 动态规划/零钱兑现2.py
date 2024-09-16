908bf77770ac167012852dc6ce83f9fd8c2da95b
junhao8851 <yangjunhao2029@163.com>
2024-09-16 23:42:24 +0800

    Add files via upload

---
+++ 动态规划/零钱兑现2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        #dp[i]表示i = amount时的总组合数
+
+        dp = [0]*(amount + 1)
+        dp[0] = 1
+
+        # 遍历物品
+        for i in range(len(coins)):
+            # 遍历背包
+            for j in range(coins[i], amount + 1):
+                dp[j] += dp[j - coins[i]]
+                
+        return dp[amount]
