1c7ac2887e916adcd527776020f4fbbd84e8ee59
junhao8851 <yangjunhao2029@163.com>
2024-10-12 15:51:12 +0800

    Add files via upload

---
+++ 动态规划/最长递增子序列.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        @cache
+        def dfs(i):
+            res = 0
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    res = max(res , dfs(j))
+            return res + 1
+
+        n = len(nums)
+        return max(dfs(i) for i in range(n))
