d38d781fb26a3d3ff533a16323fd7a3e58cfc0a6
junhao8851 <yangjunhao2029@163.com>
2024-10-15 23:19:08 +0800

    Add files via upload

--- 动态规划/最大子数组和.py
+++ 动态规划/最大子数组和.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #只要求返回最大和的值，不要求得到最大的连续子数组是哪一个，这种某个值不断尝试更新的问题通常考虑动态规划
 
-        n = len(nums)
-        if n == 0:
-            return 0
-        dp = [0 for _ in range(n)]
+        # dp[i]定义为以nums[i]结尾的最大子数组之和
 
+        # 初始化
+        dp = [0] * len(nums)
         dp[0] = nums[0]
-        for i in range(1, n):
-            if dp[i - 1] >= 0:
-                dp[i] = dp[i - 1] + nums[i]
-            else:
-                dp[i] = nums[i]
+
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1], 0) + nums[i]  # 递推公式
+
         return max(dp)
