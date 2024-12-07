85a38ba107e59fad8a5306353c83ec40d17b37f4
junhao8851 <yangjunhao2029@163.com>
2024-12-07 23:39:27 +0800

    Add files via upload

--- 贪心算法/划分字母区间.py
+++ 贪心算法/划分字母区间.py
@@ -1,16 +1,16 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last_appear= {}  # 字典存储每个字符最后出现的位置
+        last_occurrence = {}  # 存储每个字符最后出现的位置
         for i, ch in enumerate(s):
-            last_appear[ch] = i
+            last_occurrence[ch] = i
 
         result = []
         start = 0
         end = 0
         for i, ch in enumerate(s):
-            end = max(end, last_appear[ch])  # 找到当前字符出现的最远位置
-            if i == end:  # 如果当前位置是最远位置，则可以分割出一个区间
-                result.append(end - start + 1) # 把区间长度存入result
+            end = max(end, last_occurrence[ch])  # 找到当前字符出现的最远位置
+            if i == end:  # 如果当前位置是最远位置，表示可以分割出一个区间
+                result.append(end - start + 1)
                 start = i + 1
 
         return result
