d8a44c30ab07adf641165d7b93f3efc808ff2b45
junhao8851 <yangjunhao2029@163.com>
2024-12-09 23:34:03 +0800

    Delete 04_最长公共前缀.py

--- 04_最长公共前缀.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        s0=strs[0]
-        for j,c in enumerate(s0):
-            for s in strs:
-                if j==len(s) or s[j]!=c:
-                    return s0[:j]
-        return s0
