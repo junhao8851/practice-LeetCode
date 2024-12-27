c32fe2da73d78c7bd43a97dfa680df4f63e73d41
junhao8851 <yangjunhao2029@163.com>
2024-12-27 23:33:25 +0800

    Add files via upload

---
+++ 贪心算法/复习/分发糖果.py
@@ -0,0 +1,17 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candyVec = [1] * len(ratings)
+        
+        # 从前向后遍历，处理右侧比左侧评分高的情况
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candyVec[i] = candyVec[i - 1] + 1
+        
+        # 从后向前遍历，处理左侧比右侧评分高的情况
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candyVec[i] = max(candyVec[i], candyVec[i + 1] + 1)
+        
+        # 统计结果
+        result = sum(candyVec)
+        return result
