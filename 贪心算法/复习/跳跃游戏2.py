1fdd7833ee29d608454805938ebf49c91422ae3b
junhao8851 <yangjunhao2029@163.com>
2024-12-29 21:00:37 +0800

    Add files via upload

--- 贪心算法/复习/跳跃游戏2.py
+++ 贪心算法/复习/跳跃游戏2.py
@@ -1,18 +1,13 @@
 class Solution:
     def jump(self, nums):
-        if len(nums) == 1:
-            return 0
-        
-        cur_distance = 0  # 当前覆盖最远距离下标
+        cur_distance = 0  # 当前覆盖的最远距离下标
         ans = 0  # 记录走的最大步数
-        next_distance = 0  # 下一步覆盖最远距离下标
+        next_distance = 0  # 下一步覆盖的最远距离下标
         
-        for i in range(len(nums)):
-            next_distance = max(nums[i] + i, next_distance)  # 更新下一步覆盖最远距离下标
-            if i == cur_distance:  # 遇到当前覆盖最远距离下标
-                ans += 1  # 需要走下一步
-                cur_distance = next_distance  # 更新当前覆盖最远距离下标（相当于加油了）
-                if next_distance >= len(nums) - 1:  # 当前覆盖最远距离达到数组末尾，不用再做ans++操作，直接结束
-                    break
+        for i in range(len(nums) - 1):  # 注意这里是小于len(nums) - 1，这是关键所在
+            next_distance = max(nums[i] + i, next_distance)  # 更新下一步覆盖的最远距离下标
+            if i == cur_distance:  # 遇到当前覆盖的最远距离下标
+                cur_distance = next_distance  # 更新当前覆盖的最远距离下标
+                ans += 1
         
         return ans
