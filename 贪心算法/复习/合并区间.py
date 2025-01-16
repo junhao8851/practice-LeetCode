bac40014b60d9678b030539f89081c4905d696f6
junhao8851 <yangjunhao2029@163.com>
2025-01-16 23:56:27 +0800

    Add files via upload

---
+++ 贪心算法/复习/合并区间.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda p: p[0])  # 按照左端点从小到大排序
+        ans = []
+        for p in intervals:
+            if ans and p[0] <= ans[-1][1]:  # 可以合并
+                ans[-1][1] = max(ans[-1][1], p[1])  # 更新右端点最大值
+            else:  # 不相交，无法合并
+                ans.append(p)  # 新的合并区间
+        return ans
