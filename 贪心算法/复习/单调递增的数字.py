a28cc4abbb638b5094d4be23c391ac319560c691
junhao8851 <yangjunhao2029@163.com>
2025-02-06 21:23:04 +0800

    Add files via upload

--- 贪心算法/复习/单调递增的数字.py
+++ 贪心算法/复习/单调递增的数字.py
@@ -1,32 +1,14 @@
 class Solution:
-    def monotoneIncreasingDigits(self, N: int) -> int:
-        nums = list(str(N))
-        length = len(nums)
-        begin = 0
-        # N 是否符合条件
-        is_result = True
-        max_num = float('-inf')
-        
-        # 从前往后观察
-        for i in range(1, length):
-            num = int(nums[i])
-            pre_num = int(nums[i - 1])
-            # 记录最大值
-            if pre_num > max_num:
-                begin = i - 1
-                max_num = pre_num
-            if pre_num > num:
-                is_result = False
-                break
-        
-        # 如果 N 本身符合条件，直接返回 N
-        if is_result:
-            return N
-        
-        # begin 位置减去 1，后面全部替换为 9
-        nums[begin] = str(int(nums[begin]) - 1)
-        for i in range(begin + 1, length):
-            nums[i] = '9'
-                    
-        return int("".join(nums))
+    def monotoneIncreasingDigits(self, n: int) -> int:
+        digits = [int(i) for i in list(str(n))]
+        if len(digits) == 1: return n
+
+        for i in range(len(digits)-1, 0, -1):
+            if digits[i] < digits[i - 1]:
+                digits[i:] = [9] * (len(digits) - i)
+                digits[i - 1] -= 1
+
+        res = 0
+        for num in digits: res = res * 10 + num
+        return res
 
