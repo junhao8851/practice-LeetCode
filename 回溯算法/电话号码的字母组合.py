02b0884ed3626d9fece3ef4a446350c2a39a478c
junhao8851 <yangjunhao2029@163.com>
2025-03-10 23:39:07 +0800

    Add files via upload

--- 回溯算法/电话号码的字母组合.py
+++ 回溯算法/电话号码的字母组合.py
@@ -1,25 +1,13 @@
-class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits: 
+        if len(digits) == 0:
             return []
+        result = [""]
+        dict = {2: ['a','b','c'], 3:['d','e','f'], 4: ['g','h','i'], 5: ['j','k','l'], 6: ['m', 'n', 'o'], 7: ['p', 'q', 'r', 's'], 8: ['t', 'u' , 'v'], 9: ['w', 'x', 'y', 'z']}
+        for item in digits:
+            temp_list = []
+            for i in dict[int(item)]:
+                for j in result:
+                    temp_list.append(j + i)
+            result = temp_list
+        return result
 
-        phone = {'2':['a','b','c'],
-                 '3':['d','e','f'],
-                 '4':['g','h','i'],
-                 '5':['j','k','l'],
-                 '6':['m','n','o'],
-                 '7':['p','q','r','s'],
-                 '8':['t','u','v'],
-                 '9':['w','x','y','z']}
-
-        res = []  
-
-        def backtrack(conbination,next_digit):
-            if len(next_digit) == 0:
-                res.append(conbination)
-            else:
-                for letter in phone[next_digit[0]]:
-                    backtrack(conbination + letter,next_digit[1:])
-
-        backtrack('',digits)
-        return res
